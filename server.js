44e8b8f69a6b37e14c7f63bf07e5f03dae125cff
andresfelipe9619 <andresfelipe9619@gmail.com>
2019-02-24 19:31:45 -0500

    Read qr and display form with the user data

--- server.js
+++ server.js
@@ -2,11 +2,7 @@ var GENERAL_DB =
   "https://docs.google.com/spreadsheets/d/1108Pbaw4SD_Cpx2xc6Q7x1UvrET0SsPgNNZOPumt9Gg/edit#gid=0";
 
 function doGet(request) {
-  //   var isAdmin = validateUserSession();
-  //   var isAttendant = readRequestParameter(request);
-  //   if (isAdmin)
   return createHtmlTemplate("index.html");
-  //   return createHtmlTemplate("close.html");
 }
 function createHtmlTemplate(filename) {
   return HtmlService.createTemplateFromFile(filename)
@@ -14,17 +10,113 @@ function createHtmlTemplate(filename) {
     .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
 
-function validateUserSession() {
-  var guess_email = Session.getActiveUser().getEmail();
-  if (
-    guess_email == "suarez.andres@correounivalle.edu.co" ||
-    guess_email == "samuel.ramirez@correounivalle.edu.co"
-  ) {
-    return true;
-  }
-  return false;
-}
-
 function include(filename) {
   return HtmlService.createHtmlOutputFromFile(filename).getContent();
+}
+
+function searchPerson(cedula) {
+  var person = validatePerson(cedula);
+  logFunctionOutput(searchPerson.name, person);
+  return person;
+}
+
+function validatePerson(cedula) {
+  var inscritos = getPeopleRegistered();
+  var result = {
+    isRegistered: false,
+    index: -1,
+    data: null
+  };
+  for (var person in inscritos) {
+    if (String(inscritos[person].cedula) === String(cedula)) {
+      result.isRegistered = true;
+      result.index = person;
+      result.data = inscritos[person];
+    }
+  }
+  logFunctionOutput(validatePerson.name, result);
+
+  if (result.index < 0) {
+    result.isRegistered = false;
+  }
+  return JSON.stringify(result);
+}
+
+function getPeopleRegistered() {
+  var peopleSheet = getRawDataFromSheet(GENERAL_DB, "INSCRITOS");
+  var peopleObjects = sheetValuesToObject(peopleSheet);
+  // logFunctionOutput(getPeopleRegistered.name, peopleObjects)
+  return peopleObjects;
+}
+
+function getRawDataFromSheet(url, sheet) {
+  var mSheet = getSheetFromSpreadSheet(url, sheet);
+  if (mSheet)
+    return mSheet.getSheetValues(
+      1,
+      1,
+      mSheet.getLastRow(),
+      mSheet.getLastColumn()
+    );
+}
+
+function getSheetFromSpreadSheet(url, sheet) {
+  var Spreedsheet = SpreadsheetApp.openByUrl(url);
+  if (url && sheet) return Spreedsheet.getSheetByName(sheet);
+}
+
+function sheetValuesToObject(sheetValues, headers) {
+  var headings = headers || sheetValues[0].map(String.toLowerCase);
+  if (sheetValues) var people = sheetValues.slice(1);
+  var peopleWithHeadings = addHeadings(people, headings);
+
+  function addHeadings(people, headings) {
+    return people.map(function(personAsArray) {
+      var personAsObj = {};
+
+      headings.forEach(function(heading, i) {
+        personAsObj[heading] = personAsArray[i];
+      });
+
+      return personAsObj;
+    });
+  }
+  // logFunctionOutput(sheetValuesToObject.name, peopleWithHeadings)
+  return peopleWithHeadings;
+}
+
+function logFunctionOutput(functionName, returnValue) {
+  Logger.log("Function-------->" + functionName);
+  Logger.log("Value------------>");
+  Logger.log(returnValue);
+  Logger.log("----------------------------------");
+}
+
+function objectToSheetValues(object, headers) {
+  var arrayValues = new Array(headers.length);
+  var lowerHeaders = headers.map(function(item) {
+    return item.toLowerCase();
+  });
+
+  for (var item in object) {
+    for (var header in lowerHeaders) {
+      if (String(object[item].name) == String(lowerHeaders[header])) {
+        if (
+          object[item].name == "nombres" ||
+          object[item].name == "apellidos" ||
+          object[item].name == "institucion" ||
+          object[item].name == "nombre_ponencia" ||
+          object[item].name.indexOf("autor") !== -1
+        ) {
+          arrayValues[header] = object[item].value.toUpperCase();
+          Logger.log(arrayValues);
+        } else {
+          arrayValues[header] = object[item].value;
+          Logger.log(arrayValues);
+        }
+      }
+    }
+  }
+  //logFunctionOutput(objectToSheetValues.name, arrayValues)
+  return arrayValues;
 }
